ea97fe3abcd0a7b34fff3c3a1363d8893568144a
shashikantmaurya0007 <80745766+shashikantmaurya0007@users.noreply.github.com>
2021-09-27 12:12:33 +0530

    final

--- index.js
+++ index.js
@@ -12,19 +12,16 @@ function returnTheSum(date) {
 
     let sum = 0;
     dmy.map((a) => {
-
-        a.split('').map((c) => {
+        a.split("").map((c) => {
             sum += parseInt(c);
-        })
-
+        });
     });
     return sum;
 }
 
 function checkLucky(sum, luckyNumber) {
     console.log(sum, luckyNumber);
-    if (sum % luckyNumber == 0)
-        return true;
+    if (sum % luckyNumber == 0) return true;
 
     return false;
 }
@@ -32,22 +29,22 @@ function checkLucky(sum, luckyNumber) {
 function validate(event) {
     event.preventDefault();
     let date_value = document.getElementById("date").value;
-    let luckyNumber_value = parseInt(document.getElementById("luckyNumber").value);
+    let luckyNumber_value = parseInt(
+        document.getElementById("luckyNumber").value
+    );
     let sum = returnTheSum(date_value);
 
-    if (checkLucky(sum, luckyNumber_value)) {
+    if (!checkLucky(sum, luckyNumber_value)) {
         console.log("true");
         document.getElementById("result").style.visibility = "visible";
         document.getElementById("result").style.display = "block";
         document.getElementById("happy").style.visibility = "hidden";
     } else {
-
         document.getElementById("result").style.display = "none";
         document.getElementById("happy").style.visibility = "visible";
         console.log("false");
     }
 }
-
 
 document.getElementById("happy").style.visibility = "hidden";
 document.getElementById("result").style.visibility = "hidden";
